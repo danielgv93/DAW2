aa42ec4b27f38ea71f95b84df114b074f29d8ee6
Daniel <lequodo@gmail.com>
2020-09-28 18:30:14 +0200

    Realizacion de los metodos a mano. Correccion

--- DWES/Hoja03_PHP_03/Ej1.php
+++ DWES/Hoja03_PHP_03/Ej1.php
@@ -9,34 +9,69 @@
 
 <body>
     <?php
-    
+
     $array1 = array();
     $array2 = array();
     $arrayTotal = array();
-    
-    for ($i = 0; $i < 20; $i++) {
-        $array1[$i] = ($i+1);
+
+    // FUNCIONES
+    function cargarArray($tamañoArray)
+    {
+        for ($i = 0; $i < $tamañoArray; $i++) {
+            $array[$i] = rand(0, 50);
+        }
+        return $array;
     }
-    for ($i = 0; $i < 20; $i++) {
-        $array2[$i] = ($i+1);
+
+    function mostrarArray($array)
+    {
+        foreach ($array as $element) {
+            print ($element) . " ";
+        }
     }
 
-    print_r($array1);
+    function ordenar($array)
+    {
+        do {
+            $boolean = false;
+            for ($i = 0; $i < count($array) - 1; $i++) {
+                if ($array[$i] > $array[$i + 1]) {
+                    list($array[$i + 1], $array[$i]) =
+                        array($array[$i], $array[$i + 1]);
+                    $boolean = true;
+                }
+            }
+        } while ($boolean);
+        return $array;
+    }
+
+    function mezclarArrays($array1, $array2)
+    {
+        foreach ($array2 as $item) {
+            $array1[] = $item;
+        }
+        return $array1;
+    }
+
+    $array1 = cargarArray(20);
+    $array2 = cargarArray(20);
+
+    mostrarArray($array1);
     echo "<br>";
-    print_r($array2);
+    mostrarArray($array2);
     echo "<br>";
-    
-    $arrayTotal = array_merge($array1, $array2);
+
+    $arrayTotal = mezclarArrays($array1, $array2);
     echo "<br>";
     echo "<br>";
 
-    print_r($arrayTotal);
+    mostrarArray($arrayTotal);
 
-    sort($arrayTotal);
+    $arrayTotal = ordenar($arrayTotal);
     echo "<br>";
     echo "<br>";
 
-    print_r($arrayTotal);
+    mostrarArray($arrayTotal);
 
     ?>
 </body>
