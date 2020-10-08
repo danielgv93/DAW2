6668d382258a72535b5a3ce2ee14550b2dbd5f48
Dani - Clase <lequodo@gmail.com>
2020-10-08 18:36:53 +0200

    Parte 2 de los ejercicios

--- DWEC/Tema2/EjerciciosBase/Ej16.js
+++ DWEC/Tema2/EjerciciosBase/Ej16.js
@@ -1,4 +1,7 @@
 let input = 0;
+let intentos = 0;
 do {
     input = parseInt(prompt("Introduzca numeros"));
-} while (input != NaN);
+    intentos++;
+} while (!isNaN(input));
+alert(`Numero de intentos: ${intentos}`);
