b4cdfe08a56261bf3bc149d8cc987cb642d759ee
Daniel <lequodo@gmail.com>
2020-10-09 18:32:33 +0200

    Cambiar la logica de los arrays y maquetar

--- DWES/Hoja03_PHP_05/Ej4.php
+++ DWES/Hoja03_PHP_05/Ej4.php
@@ -1,43 +1,27 @@
 <?php
 
-$listaPeliculas = array(
-    "your name", "1917", "joker", "el hoyo", "scarface",
-    "jojo rabit", "fast and furious", "el camino", "gula", "psicosis"
+$listadoPeliculas = array(
+    array("titulo" => "your name", "img" => "img/yourname.jpg"),
+    array("titulo" => "1917", "img" => "img/1917.jpg"),
+    array("titulo" => "joker", "img" => "img/joker.png"),
+    array("titulo" => "El hoyo", "img" => "img/elhoyo.jpg"),
+    array("titulo" => "scarface", "img" => "img/scarface.jpg"),
+    array("titulo" => "jojo rabit", "img" => "img/jojorabbit.jpg"),
+    array("titulo" => "fast and furious", "img" => "img/fastandfurious.jpg"),
+    array("titulo" => "el camino", "img" => "img/elcamino.jpg"),
+    array("titulo" => "el hobbit", "img" => "img/elhobbit.jpg"),
+    array("titulo" => "psicosis", "img" => "img/psicosis.jpg")
 );
-$listaImagenes = array(
-    "img/yourname.jpg", "img/1917.jpg", "img/joker.png", "img/elhoyo.jpg",
-    "scarface.jpg", "img/jojorabit.jpg", "img/fastandfurious.jpg", "img/elcamino.jpg",
-    "img/gula.jpg", "img/psicosis.jpg"
-);
-
-function posBusqueda($arrayPeliculas)
-{
-    $posBusqueda = array();
-    if (isset($_REQUEST["pelicula"])) {
-        $busqueda = $_REQUEST["pelicula"];
-
-        for ($i = 0; $i < count($arrayPeliculas); $i++) {
-            if (strpos($arrayPeliculas[$i], $busqueda) === false) {
-            } else {
-                $posBusqueda[] = $i;
-            };
+$resultado = array();
+$busqueda = "";
+if (isset($_POST["pelicula"])) {
+    $busqueda = $_REQUEST["pelicula"];
+    foreach ($listadoPeliculas as $pelicula) {
+        if (strpos($pelicula["titulo"], $busqueda) !== false) {
+            $resultado[] = $pelicula;
         }
-        return $posBusqueda;
     }
 }
-
-function peliculasResultado($arrayPosiciones, $arrayPeliculas, $arrayImagenes)
-{
-    $resultado = array();
-    for ($i = 0; $i < count($arrayPosiciones); $i++) {
-        $aux = array($arrayPeliculas[$arrayPosiciones[$i]] => $arrayImagenes[$arrayPosiciones[$i]]);
-        $resultado[] = $aux;
-    }
-    return $resultado;
-}
-
-$resultadoBusqueda = peliculasResultado(posBusqueda($listaPeliculas), $listaPeliculas, $listaImagenes);
-print ($resultadoBusqueda[0][0]);
 ?>
 <!DOCTYPE html>
 <html lang='es'>
@@ -46,32 +30,40 @@ print ($resultadoBusqueda[0][0]);
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-    <title>Ejercicio 3</title>
+    <title>Ejercicio 4</title>
     <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm' crossorigin='anonymous'>
 </head>
 
 <body>
     <div class="container border rounded">
-        <h2>Buscador de peliculas</h2>
-        <form action="" method="POST">
-            <div class="form-group">
-                <label for="pelicula">Buscador</label>
-                <input type="text" class="form-control" name="pelicula" id="pelicula" autofocus>
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <h2>Buscador de peliculas</h2>
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label for="pelicula">Buscador</label>
+                        <input type="text" class="form-control" name="pelicula" id="pelicula" autofocus>
+                    </div>
+                    <button name="submit" type="submit" class=" btn btn-primary">Buscar</button>
+
+
+                    <table class="table border mt-3">
+                        <tbody>
+                            <?php
+                            foreach ($resultado as $pelicula) {
+                                print("<tr scope='row'>
+                                    <td><img class='img-fluid' src='" . $pelicula["img"] . "' alt='pelicula'></td>
+                                    <td>" . $pelicula["titulo"] . "</td>
+                                    </tr>");
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+
+
+                </form>
             </div>
-            <button name="submit" type="submit" class=" btn btn-primary">Buscar</button>
-            <table class="table">
-                <tbody>
-                    <?php
-                    /* for ($i=0; $i < count($resultadoBusqueda); $i++) { 
-                        print("<tr>");
-                            print("<td><img class='img-fluid' src='".$resultadoBusqueda[$i][0]."'></td>");
-                            print("<td>". $resultadoBusqueda[$i][1]."</td>");
-                        print("</tr>");
-                    } */
-                    ?>
-                </tbody>
-            </table>
-        </form>
+        </div>
     </div>
 </body>
 <script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>
