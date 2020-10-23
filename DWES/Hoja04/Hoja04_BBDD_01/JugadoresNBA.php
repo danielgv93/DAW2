9048e8d2382e0b1b5afb0904a5bbb4ce1cc11739
Daniel <lequodo@gmail.com>
2020-10-23 18:55:21 +0200

    Clase 23-10

--- DWES/Hoja04/Hoja04_BBDD_01/JugadoresNBA.php
+++ DWES/Hoja04/Hoja04_BBDD_01/JugadoresNBA.php
@@ -1,9 +1,52 @@
 <?php
-require_once "../ConexionBD.php";
 require_once "queriesBD.php";
+if (isset($_POST["equipo"])) {
+    $equipoSelected = $_POST["equipo"];
+    $jugadoresEquipoSelected = getJugadoresDeEquipo($equipoSelected);
+}
 
-$conexionNBA = getConexion("nba");
+$nombreEquipos = getEquipos();
 
-mostrarResultados(getEquipos($conexionNBA));
+
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Jugadores de la NBA</h1>
+    <form method="POST" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+        <label for="equipo">Equipo:
+            <select name="equipo">
+                <?php while ($registro = $nombreEquipos->fetch()) : ?>
+                    <option value="<?php echo $registro["nombre"] ?>"><?php echo $registro["nombre"] ?></option>
+                <?php endwhile ?>
+            </select>
+        </label>
+
+        <input type="submit" name="mostrar" value="Mostrar">
+    </form>
+    <table>
+        <tr>
+            <th>Nombre</th>
+        </tr>
+        <?php if ($jugadoresEquipoSelected->execute()) : ?>
+
+            <?php while ($fila = $consulta->fetch()) : ?>
+                <tr>
+                    <td><?php ?></td>
+                </tr>
+                $datos[] = array("valor1" => $fila['valor1'], "valor2" => $fila['valor2']);
+
+            <?php endwhile ?>
+        <?php endif ?>
+    </table>
+</body>
+
+</html>
