1a4cbe721433dea0fbd47d635290ca899e759c91
Daniel <lequodo@gmail.com>
2020-11-10 19:35:58 +0100

    Examen terminao

--- DWES/Examen2/ejercicio1.php
+++ DWES/Examen2/ejercicio1.php
@@ -2,11 +2,32 @@
 $historico = array(
     "Torrelavega" => array("Enero" => 85, "Febrero" => 68, "Marzo" => 67, "Abril" => 86, "Mayo" => 62, "Junio" => 54, "Julio" => 39, "Agosto" => 61, "Septiembre" => 84, "Octubre" => 99, "Noviembre" => 120, "Diciembre" => 115),
     "Sevilla" => array("Enero" => 76, "Febrero" => 73, "Marzo" => 66, "Abril" => 53, "Mayo" => 34, "Junio" => 14, "Julio" => 1, "Agosto" => 3, "Septiembre" => 18, "Octubre" => 69, "Noviembre" => 87, "Diciembre" => 82),
-    "Madrid" => array("Enero" => 44, "Febrero" => 35, "Marzo" => 45, "Abril" => 44, "Mayo" => 28, "Junio" => 11, "Julio" => 11, "Agosto" => 11, "Septiembre" => 30, "Octubre" => 51, "Noviembre" => 58, "Diciembre" => 50)
+    "Madrid" => array("Enero" => 43, "Febrero" => 44, "Marzo" => 35, "Abril" => 45, "Mayo" => 44, "Junio" => 28, "Julio" => 11, "Agosto" => 11, "Septiembre" => 30, "Octubre" => 51, "Noviembre" => 58, "Diciembre" => 50)
 );
 
-if (isset($_POST["enviar"])) {
+if (isset($_POST["comprobar"])) {
     $ciudadSeleccionada = $_POST["ciudad"];
+    $precipitacion = $_POST["precipitacion"];
+    $totalCiudad = 0;
+    foreach ($historico[$ciudadSeleccionada] as $mes => $numero) {
+        $totalCiudad += $numero;
+    }
+    $mediaCiudad = $totalCiudad / 12;
+    $texto = crearTexto($mediaCiudad, $precipitacion, $ciudadSeleccionada);
+}
+
+function crearTexto($mediaCiudad, $precipitacion, $ciudadSeleccionada)
+{
+    $texto = "La precipitacion media anual de $ciudadSeleccionada es ";
+    if ($mediaCiudad < $precipitacion) {
+        $texto .= "MAYOR ";
+    } else if ($mediaCiudad > $precipitacion) {
+        $texto .= "MENOR ";
+    } else {
+        $texto .= "IGUAL ";
+    }
+    $texto .= "que $precipitacion. Concretamente $mediaCiudad";
+    return $texto;
 }
 
 ?>
@@ -24,17 +45,23 @@ if (isset($_POST["enviar"])) {
 <form action="#" method="post">
 
     <?php foreach ($historico as $ciudad => $precipitaciones) : ?>
-            
-                <label for="<?= $ciudad ?>">
-                    <input type="radio" id="<?= $ciudad ?>" <?= $checkedProp = (isset($ciudadSeleccionada) &&
-                                                                                            $ciudadSeleccionada == $ciudad) ? "checked" : ""; ?> name="ciudad" value="<?= $posicion ?>">
-                    <?= $posicion ?></label>
-           
-        <?php endforeach ?>
 
-    
+        <label for="<?= $ciudad ?>">
+            <input type="radio" id="<?= $ciudad ?>" <?= $checkedProp = (isset($ciudadSeleccionada) &&
+                $ciudadSeleccionada == $ciudad) ? "checked" : ""; ?> name="ciudad" value="<?= $ciudad ?>">
+            <?= $ciudad ?></label>
+    <?php endforeach ?>
+
+    <br>
+    Precipitación Media:<br>
+    <input type="number" name="precipitacion" step="any">
+    <input type="submit" name="comprobar" value="Comprobar">
 
 </form>
-    
+    <?php
+    if (isset($_POST["comprobar"])) {
+        echo $texto;
+    }
+    ?>
 </body>
 </html>
