a7cbf65ed533930c9c222325d1bf39022168278a
Daniel <lequodo@gmail.com>
2020-11-10 17:01:39 +0100

    Terminar mercadillo

--- DWEC/Tema3/ArraysAsociativos/mercadillo.js
+++ DWEC/Tema3/ArraysAsociativos/mercadillo.js
@@ -1,19 +1,25 @@
 addEventListener('load', inicio, false);
 
 function inicio() {
-    let tienduca = {"tomate": 5, "lechuga": 10, "manzanas": 14};
+    let tienduca = {"tomate": 5, "lechuga": 10, "manzana": 14};
 
     let textoLabel = document.getElementById("labelTexto");
     let nombreInput = document.getElementById("inputNombre");
     let stockInput = document.getElementById("inputStock");
+    let addBoton = document.getElementById("botonAñadir");
+    addBoton.addEventListener("click", function () {
+        add(tienduca, nombreInput.value, stockInput.value);
+        textoLabel.value = `Se han añadido ${nombreInput.value} con un stock de ${stockInput.value}`;
+    }, false);
     let venderBoton = document.getElementById("botonVender");
-    venderBoton.addEventListener("click", function (){
+    venderBoton.addEventListener("click", function () {
         if (vender(tienduca, nombreInput.value, stockInput.value)) {
-            textoLabel.innerHTML = `Quedan ${tienduca[nombreInput.value]} ${nombreInput.value+"s"}`;
+            textoLabel.value = `Quedan ${tienduca[nombreInput.value]} ${nombreInput.value + "s"}`;
         }
     }, false)
 
 }
+
 function vender(arrayProductos, nombre, stock) {
     if (arrayProductos[nombre] >= stock) {
         arrayProductos[nombre] -= stock;
@@ -23,4 +29,16 @@ function vender(arrayProductos, nombre, stock) {
         alert("Error");
         return false;
     }
+}
+
+function add(arrayProductos, nombre, stock) {
+    arrayProductos[nombre] = stock;
+}
+
+function visualizar(arrayProductos, texto) {
+    texto = "";
+    for (const nombre in arrayProductos) {
+        texto.concat(`${nombre} -- ${arrayProductos[nombre]} <br>`);
+    }
+    return texto;
 }
