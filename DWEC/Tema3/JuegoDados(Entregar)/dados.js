b13eef2590b41e312732aaebcf0f0b2943d09652
Daniel Garcia <lequodo@gmail.com>
2020-11-16 02:46:33 +0100

    Finalizar. Listo para entregar

--- DWEC/Tema3/JuegoDados(Entregar)/dados.js
+++ DWEC/Tema3/JuegoDados(Entregar)/dados.js
@@ -7,10 +7,9 @@ function inicio() {
     let tiradaInput = document.getElementById("tirada");
     let usuario = "";
     let tirada = 1;
-    let nJugadores = 0;
     let start = false;
     let puntaciones = Array(maxTiradas);
-    let clasificacion = Array();
+    let clasificacion = {};
     let usuarioBoton = document.getElementById("comenzarBoton");
     let textArea = document.getElementById("textArea");
     usuarioBoton.addEventListener("click", function () {
@@ -18,7 +17,6 @@ function inicio() {
             usuario = prompt("Introduzca su nombre de usuario");
             puntaciones.fill(0);
             start = true;
-            nJugadores++;
             tiradaInput.value = 0;
             for (const arraySumaInputElement of arraySumaInput) {
                 arraySumaInputElement.value = 0;
@@ -31,27 +29,27 @@ function inicio() {
     botonInput.addEventListener("click", function () {
         if (start) {
             tiradaInput.value = tirada;
-            if (tirada <= maxTiradas-2) {
+            if (tirada <= maxTiradas - 2) {
                 let sumaTirada = tiradaDados(arrayDadosURL, tirada);
                 arraySumaInput[tirada - 1].value = sumaTirada;
                 puntaciones[tirada - 1] += sumaTirada;
-                clasificacion[usuario] = puntaciones;
+                clasificacion[usuario] = puntaciones.slice();
                 tirada++;
             } else if (tirada <= maxTiradas) {
-                let suma = 0;
-                let dados = Array(5).fill(2);
+                let dados = Array();
 
-                /*for (let i = 0; i < document.images.length; i++) {
+                for (let i = 0; i < document.images.length; i++) {
                     dados.push(Math.round(Math.random() * 5 + 1));
                     document.images[i].src = arrayDadosURL[dados[i] - 1];
-                }*/
-                if (getNDuplicados(dados) === 4 && tirada === maxTiradas-1) {
+                }
+                if (getNDuplicados(dados) === 4 && tirada === maxTiradas - 1) {
                     arraySumaInput[tirada - 1].value = 60;
-                    clasificacion[usuario] = Array(60);
+                    puntaciones[tirada - 1] += 60;
+                    clasificacion[usuario] = puntaciones.slice();
                 } else if (getNDuplicados(dados) === 5 && tirada === maxTiradas) {
                     arraySumaInput[tirada - 1].value = 80;
                     puntaciones[tirada - 1] += 80;
-                    clasificacion[usuario] = puntaciones;
+                    clasificacion[usuario] = puntaciones.slice();
                 }
                 tirada++;
             } else {
@@ -62,10 +60,11 @@ function inicio() {
         } else {
             alert("Haga click en 'Empezar a jugar'");
         }
+        textArea.value = getClasificacion(clasificacion);
     }, false);
     let mostrarClasificacion = document.getElementById("mostrarBoton");
     mostrarClasificacion.addEventListener("click", function () {
-        textArea.value = getClasificacion(clasificacion, nJugadores);
+        textArea.value = getClasificacion(clasificacion);
     }, false);
 }
 
@@ -81,26 +80,40 @@ function tiradaDados(arrayDadosURL, tirada) {
     return suma;
 }
 
-function getClasificacion(clasificacion, nJugadores) {
+function getClasificacion(clasificacion) {
     let texto = "";
-    let usuarios = Object.keys(clasificacion);
-    for (let i = 0; i < nJugadores; i++) {
-        texto += `${usuarios[i]}: ${clasificacion[usuarios[i]].reduce((a, b) => a + b)}\n`;
-    }
+    let aux = [];
+    for (const clasificacionKey in clasificacion) aux.push([clasificacionKey, clasificacion[clasificacionKey]]);
 
-    /*for (const nombreUsuario in clasificacion) {
-        texto += `${nombreUsuario}: ${clasificacion[nombreUsuario].reduce((a, b) => a + b)}\n`;
-    }*/
+    aux = aux.sort(ordenarClasificacion);
+
+    for (const elemento of aux) {
+        texto += `${elemento[0]}: ${elemento[1].reduce((a, b) => a + b)}\n`;
+    }
     return texto;
 }
 
 function getNDuplicados(array) {
     let contador = 0;
-    array = array.sort(function(a, b){return a-b});
+    array = array.sort((a, b) => a - b);
     for (let i = 0; i < array.length - 1; i++) {
-        if (array[i] === array[i+1]) {
+        if (array[i] === array[i + 1]) {
             contador++;
         }
     }
-    return contador +1;
+    return contador + 1;
+}
+
+function ordenarClasificacion(a, b) {
+    let valorA = a[1].reduce((x, y) => x + y);
+    let valorB = b[1].reduce((x, y) => x + y);
+    let resultado;
+    if (valorA > valorB) {
+        resultado = -1;
+    } else if (valorA < valorB) {
+        resultado = 1;
+    } else {
+        resultado = 0;
+    }
+    return resultado;
 }
