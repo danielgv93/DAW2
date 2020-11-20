e54c2ea5726054d862882220309214768fdaf953
Daniel <lequodo@gmail.com>
2020-11-20 20:11:21 +0100

    Añadir usuarios a la base de datos

--- DWES/Hoja05/Hoja05_Sesiones_01/PDO/funcionesPDO.php
+++ DWES/Hoja05/Hoja05_Sesiones_01/PDO/funcionesPDO.php
@@ -130,3 +130,27 @@ function usuarioCorrecto($usuario, $password)
     }
     return count($datos);
 }
+
+function addUsuario($usuario, $password)
+{
+    $conexion = getConexionPDO();
+    try {
+        $sql = "INSERT INTO usuarios (usuario, password) VALUES (?,MD5(?));";
+        $consulta = $conexion->prepare($sql);
+        $consulta->bindParam(1, $usuario);
+        $consulta->bindParam(2, $password);
+        if ($consulta->execute()) {
+            unset($conexion);
+            return true;
+        } else{
+            throw new Exception("Error al insertar usuario");
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        unset($conexion);
+        return false;
+    }
+
+}
+
+;
