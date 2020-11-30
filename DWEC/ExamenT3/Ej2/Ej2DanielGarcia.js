f8554b7704ead9dd02b6b883803f701893a64583
Daniel <lequodo@gmail.com>
2020-11-30 16:41:22 +0100

    Corregir examen

--- DWEC/ExamenT3/Ej2/Ej2DanielGarcia.js
+++ DWEC/ExamenT3/Ej2/Ej2DanielGarcia.js
@@ -13,21 +13,11 @@ function inicio() {
 
 
 let letras = (texto) => {
-    let regexp = /[A-Z]/g;
-    // Comprobamos si hay coincidencias de MAYUS
-    let matches_array = texto.match(regexp);
-    // Si es null quiere decir que no hay ninguna MAYUS. Comprobamos si es exclusivamente MINUS
-    if (matches_array == null) {
-        regexp = /[a-z]/g;
-        matches_array = texto.match(regexp);
-        if (matches_array.length === texto.length) {
-            return `Cadena formada por MINUSCULAS`;
-        }
+    if (texto === texto.toLowerCase()) {
+        return `Texto en MINUSCULAS`;
+    } else if (texto === texto.toUpperCase()) {
+        return `Texto en MAYUSCULAS`;
+    } else {
+        return `Texto mezclao`;
     }
-    // Comprobamos si es exclusivamente MAYUS
-    if (matches_array.length === texto.length) {
-        return `Cadena formada por MAYUSCULAS`;
-    }
-    // Si es mezcla de MAYUS y MINUS
-    return `Cadena formada por MAYUSCULAS y MINUSCULAS`;
 }
