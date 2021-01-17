590c7ae315965756dbeeb1f07f52cf1b7e5e31d7
Daniel Garcia <lequodo@gmail.com>
2021-01-17 15:23:06 +0100

    Terminar hoja

--- DWEC/Tema6/Creacion Nodos/Ej7.js
+++ DWEC/Tema6/Creacion Nodos/Ej7.js
@@ -5,22 +5,77 @@ function inicio() {
     let textoDato = document.getElementById("dato");
     let select = document.getElementById("select");
     let borrarElementoBoton = document.getElementById("borrarElemento");
+    let borrarTodoBoton = document.getElementById("borrarTodo");
+    let addBetweenBoton = document.getElementById("addBetweenBoton");
+    let addBetweenTexto = document.getElementById("addBetweenTexto");
     let ul = document.body.children[1];
-    let arrayDatos = Array();
+
     botonAddDato.addEventListener("click", function () {
-        arrayDatos.push(textoDato.value);
-        let option = crearOption(arrayDatos[arrayDatos.length-1], arrayDatos[arrayDatos.length-1]);
-        select.appendChild(option);
-        let li = crearLi(arrayDatos[arrayDatos.length-1]);
-        ul.appendChild(li);
+        crearOption_Li(textoDato, select, ul);
+        deshabilitarBotones(false, borrarElementoBoton, borrarTodoBoton);
+
     }, false);
+
     borrarElementoBoton.addEventListener("click", function () {
-        
+        borrarUltimo(ul, select, ul.lastElementChild, select.lastElementChild);
+        if (select.childElementCount === 0) {
+            deshabilitarBotones(true, borrarElementoBoton, borrarTodoBoton);
+        }
+
+    }, false);
+
+    borrarTodoBoton.addEventListener("click", function () {
+        let aux = true;
+        do {
+            aux = borrarUltimo(ul, select, ul.lastElementChild, select.lastElementChild);
+        } while (aux);
+        deshabilitarBotones(true, borrarElementoBoton, borrarTodoBoton);
+    }, false);
+
+    addBetweenBoton.addEventListener("click", function () {
+        insertarAntesDe(ul, select, textoDato, addBetweenTexto);
+        deshabilitarBotones(false, borrarElementoBoton, borrarTodoBoton);
     }, false);
 }
 
-function f() {
-    
+function insertarAntesDe(ul, select, texto, etiquetaContenido) {
+    let li = crearLi(texto.value);
+    let option = crearOption(texto.value, texto.value);
+    for (let child of select.children) {
+        if (child.innerHTML === etiquetaContenido.value) {
+            select.insertBefore(option, child);
+            break;
+        }
+    }
+    for (let child of ul.children) {
+        if (child.innerHTML === etiquetaContenido.value) {
+            ul.insertBefore(li, child);
+            break;
+        }
+    }
+}
+
+function deshabilitarBotones(boolean) {
+    for (const argument of arguments) {
+        argument.disabled = boolean;
+    }
+}
+
+function borrarUltimo(ul, select, li, option) {
+    try {
+        ul.removeChild(li);
+        select.removeChild(option);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+function crearOption_Li(textoDato, select, ul) {
+    let option = crearOption(textoDato.value, textoDato.value);
+    select.appendChild(option);
+    let li = crearLi(textoDato.value);
+    ul.appendChild(li);
 }
 
 function crearOption(valor, texto) {
