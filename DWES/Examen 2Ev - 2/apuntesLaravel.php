44f565fe8d2787590cb8283cacf152707dc0e907
Daniel Garcia <lequodo@gmail.com>
2021-02-09 13:48:47 +0100

    arreglar apuntes 2.0

--- DWES/Examen 2Ev - 2/apuntesLaravel.php
+++ DWES/Examen 2Ev - 2/apuntesLaravel.php
@@ -1,6 +1,7 @@
 <?php
 
 // RELLENAR TABLA INTERMEDIA CON ATTACH
+$animal = array("especie"=>"leon", ".....");
 $a = new Animal();
 $a->especie = $animal["especie"];
 $a->slug = Str::slug($animal["especie"]);
@@ -15,3 +16,18 @@ $a->cuidadores()->attach([
     Cuidador::all()->skip(0)->take(10)->random()->id,
     Cuidador::all()->skip(10)->take(10)->random()->id,
 ]);
+
+
+
+// Actualizar con imagen
+$animal = new Animal();
+$animal->especie = $request->especie;
+$animal->peso = $request->peso;
+$animal->altura = $request->altura;
+$animal->fechaNacimiento = $request->fechaNacimiento;
+$animal->alimentacion = $request->alimentacion;
+$animal->descripcion = $request->descripcion;
+if ($request->imagen !== null) {
+    $animal->imagen = $request->imagen->storeAs("img", $request->imagen->getClientOriginalName());
+}
+$animal->save();
